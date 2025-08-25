fc66f1685199fa4690250b4a093a3ca1b85dac1c
JAHAGANAPATHI SUGUMAR <125806337+JahaganapathiSugumar@users.noreply.github.com>
2025-08-25 14:52:13 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, jsonify
+import joblib
+
+app = Flask(__name__)
+
+# Load model and vectorizer
+model = joblib.load('D:/Combo_Square/Frontend_connection/Models/sentiment_classifier.pkl')
+vectorizer = joblib.load('D:/Combo_Square/Frontend_connection/Models/tfidf_vectorizer.pkl')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    text = request.form['text']
+    text_vectorized = vectorizer.transform([text])
+    prediction = model.predict(text_vectorized)[0]
+    return jsonify({'sentiment': prediction})
+
+if __name__ == '__main__':
+    app.run(debug=True)
